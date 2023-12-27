4041e6c7ac6c83531d5a26d81730e7bf50af6ea5
ngcdan <linuss1908@gmail.com>
2023-12-27 17:16:50 +0700

    complete chapter 6

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -2,11 +2,23 @@ import SideNav from '@/app/ui/dashboard/sidenav';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
-      <div className="w-full flex-none md:w-64">
-        <SideNav />
+    <div className='flex flex-col'>
+      <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+        <div className="w-full flex-none md:w-64">
+          <SideNav />
+        </div>
+        <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
       </div>
-      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+
+      <footer className="flex font-mono justify-center items-center h-16 bg-stone-100 text-stone-500 text-sm">
+        <div className="flex justify-between items-center w-full px-4">
+          <p> <a rel="me" href="https://masto.ai/@dan">🐘 @dan</a> </p>
+          <div className="font-normal">
+            <a href="/rss.xml">📮 RSS</a>
+          </div>
+
+        </div>
+      </footer>
     </div>
   );
 }
