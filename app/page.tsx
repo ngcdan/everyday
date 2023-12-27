870b493a659cf07545a4af02783479db1fbf728c
ngcdan <linuss1908@gmail.com>
2023-12-27 17:18:35 +0700

    complete chapter 6

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,3 @@
-import AcmeLogo from '@/app/ui/acme-logo';
-import styles from '@/app/ui/home.module.css';
-
-import { ArrowRightIcon } from '@heroicons/react/24/outline';
-import Link from 'next/link';
-import Image from 'next/image'
 import { lusitana } from '@/app/ui/fonts';
 
 export default function Page() {
