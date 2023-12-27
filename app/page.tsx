cc7bcf25eeb0a2937cb0d9de69e91443bc006cd4
ngcdan <linuss1908@gmail.com>
2023-12-27 17:33:37 +0700

    complete chapter 6

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import { lusitana } from '@/app/ui/fonts';
 
 export default function Page() {
   return (
-    <div className='flex flex-col justify-center align-center'>
-      <h1 className={`flex justify-center text-xl ${lusitana.className}`}>Hello! I am Đàn!</h1>
+    <div className='container mx-auto flex flex-col items-center justify-center'>
+      <h1 className={`flex justify-center text-xl pt-8 ${lusitana.className}`}>Hello! I&apos;m Đàn!</h1>
       <p className="flex justify-center">Look like you&apos;ve found my space on the internet.</p>
     </div>
   );
