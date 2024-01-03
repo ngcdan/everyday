553b3c574f24cb043d0a1d02ee85d15f323eec24
ngcdan <linuss1908@gmail.com>
2024-01-03 08:56:37 +0700

    update auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import '@/app/ui/global.css'
 import { inter } from '@/app/ui/fonts'
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Le Ngoc Dan',
+    default: 'Le Ngoc Dan',
+  },
+  description: 'The official Next.js Course Dashboard, built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 
 export default function RootLayout({
   children,
