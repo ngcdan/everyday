2ede0850d69d8e63b83779bf8be7628de97f5802
ngcdan <linuss1908@gmail.com>
2024-01-24 20:13:28 +0700

    clean-code

--- auth.config.ts
+++ auth.config.ts
@@ -7,16 +7,13 @@ export const authConfig = {
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
-      console.log(auth);
-      console.log(isLoggedIn);
-
-      const isOnDashboard = nextUrl.pathname.startsWith('/');
-      if (isOnDashboard) {
-        if (isLoggedIn) return true;
-        return false; // Redirect unauthenticated users to login page
-      } else if (isLoggedIn) {
-        return Response.redirect(new URL('/', nextUrl));
-      }
+      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+      // if (isOnDashboard) {
+      //   if (isLoggedIn) return true;
+      //   return false; // Redirect unauthenticated users to login page
+      // } else if (isLoggedIn) {
+      //   return Response.redirect(new URL('/', nextUrl));
+      // }
       return true;
     },
   },
