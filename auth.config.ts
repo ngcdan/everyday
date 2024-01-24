c6d6c87cdecad2bb07e63ac3996614b3665a14df
ngcdan <linuss1908@gmail.com>
2024-01-24 19:58:32 +0700

    clean-code

--- auth.config.ts
+++ auth.config.ts
@@ -2,12 +2,13 @@ import type { NextAuthConfig } from 'next-auth';
 
 export const authConfig = {
   pages: {
-    signIn: '/',
+    // signIn: '/',
   },
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
-      const isOnDashboard = nextUrl.pathname.startsWith('/');
+      console.log(auth);
+      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
       if (isOnDashboard) {
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
@@ -17,6 +18,6 @@ export const authConfig = {
       return true;
     },
   },
-  providers: [], // Add providers with an empty array for now
+  providers: [], // Add providers with an empty array for now, different login options.
   secret: process.env.NEXT_PUBLIC_SECRET,
 } satisfies NextAuthConfig;
