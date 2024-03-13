58195048f15cc734b582e75dc481bf5414c0f842
ngcdan <linuss1908@gmail.com>
2024-03-13 17:16:30 +0700

    update code

--- app/text-editor/page.tsx
+++ app/text-editor/page.tsx
@@ -1,19 +1,22 @@
-
 import { Metadata } from 'next';
-import { Editor } from '../ui/Editor';
+import { Editor } from '../ui/components/Editor';
 
 export const metadata: Metadata = {
-  title: 'dev',
+  title: 'Text Editor',
 };
 
 export default function Page() {
   return (
-    <div className='px-4 md:px-8 lg:px-12'>
-      <h2 className='my-5 text-2xl font-bold text-left text-gray-800'>Rich Text Editor</h2>
-      <p className='my-5 text-gray-600'>
+    <div className="px-4 md:px-8 lg:px-12">
+      <h2 className="my-5 text-left text-2xl font-bold text-gray-800">
+        Rich Text Editor
+      </h2>
+      <p className="my-5 text-gray-600">
         Try to building Rich Text Editor with Lexical
       </p>
-      <Editor />
+      <div className="flex w-full">
+        <Editor />
+      </div>
     </div>
   );
 }
--- app/ui/components/Editor.tsx
+++ app/ui/components/Editor.tsx
@@ -1,11 +1,5 @@
-/**
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- *
- */
 "use client"
+
 import * as React from 'react';
 import { AutoFocusPlugin } from '@lexical/react/LexicalAutoFocusPlugin';
 import { LexicalComposer } from '@lexical/react/LexicalComposer';
