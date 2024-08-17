1d79c5e5eb4d6b29c027e2b82216ff1246d716a6
nqcdan <linuss1908@gmail.com>
2024-08-17 16:03:54 +0700

    update api

--- app/dev/api/route.ts
+++ app/dev/api/route.ts
@@ -1,18 +1,17 @@
 import { NextResponse } from 'next/server';
 
-// export default function handler(req, res) {
-//   if (req.method === 'GET') {
-//     res.status(200).json({ message: 'Hello from Next.js API!' });
-//   } else {
-//     res.setHeader('Allow', ['GET']);
-//     res.status(405).end(`Method ${req.method} Not Allowed`);
-//   }
-// }
-
 export function GET(_req: Request) {
   console.log('call api/dev/route');
   console.log(_req);
 
-  const data = { message: 'Hello from Next.js API with TypeScript!' };
+  const data = { message: 'Hello from Next.js API with TypeScript!, GET' };
+  return NextResponse.json(data, { status: 200 })
+}
+
+export function POST(_req: Request) {
+  console.log('call api/dev/route');
+  console.log(_req);
+
+  const data = { message: 'Hello from Next.js API with TypeScript!, POST' };
   return NextResponse.json(data, { status: 200 })
 }
